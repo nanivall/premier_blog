b23d5d1a74359171e104485967e7ed2d9c06c91b
anne <ainavall@gmail.com>
2018-11-09 11:49:22 +0100

    My Django Girls app, first commit

---
+++ test01.py
@@ -0,0 +1,8 @@
+def Salut (prenom):
+    print('Salut  ' + prenom + '!')
+
+filles = ['Anne', 'Laetita','Mar', 'Sylvie', 'Emilie', 'Florence']
+
+for prenom in filles:
+    Salut(prenom)
+    print('Suivante!')
